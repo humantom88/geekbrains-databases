f64fb1c3a5c5a739ad736540c5058afe85dd9a9a
Tom <humantom88@gmail.com>
2020-03-08 22:29:27 +0300

    Update tasks.sql

--- homework6/tasks.sql
+++ homework6/tasks.sql
@@ -1,7 +1,44 @@
 -- Домашняя работа
 
 use vk6;
- 
+
+
+-- Задание 1
+-- Исходный запрос: Выбираем друзей пользователя с двух сторон отношения дружбы
+(SELECT friend_id FROM friendship WHERE user_id = 8)
+UNION
+(SELECT user_id FROM friendship WHERE friend_id = 8);
+-- Улучшенный запрос (без UNION)
+SELECT id
+FROM users u 
+WHERE u.id IN (SELECT friend_id FROM friendship WHERE user_id = 8) OR
+	  u.id IN (SELECT user_id FROM friendship WHERE friend_id = 8);
+	  
+-- Исходный запрос: Выбираем только друзей с активным статусом
+(SELECT friend_id 
+  FROM friendship 
+  WHERE user_id = 8 AND status_id IN (
+      SELECT id FROM friendship_statuses WHERE name = 'Confirmed'
+    )
+)
+UNION
+(SELECT user_id 
+  FROM friendship 
+  WHERE friend_id = 8 AND status_id IN (
+      SELECT id FROM friendship_statuses WHERE name = 'Confirmed'
+    )
+);
+-- Улучшенный запрос (без UNION)
+SELECT DISTINCT id
+FROM users, (
+	SELECT user_id, friend_id
+	FROM friendship f 
+	WHERE (user_id = 8 OR friend_id = 8) AND status_id IN (
+		SELECT id FROM friendship_statuses WHERE name = 'Confirmed'
+	)) as stuff
+WHERE stuff.user_id = id OR stuff.friend_id = id
+
+
 -- Задание 2
 SELECT SUM(likes_count)
 FROM (
@@ -14,7 +51,7 @@ FROM (
 	ORDER BY p.birthday DESC
 	LIMIT 10
 ) as stuff;
--- 
+
 
 -- Задание 3
 SELECT result_table.gender, SUM(result_table.likes_count) as summary
